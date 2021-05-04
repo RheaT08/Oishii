747cce0d8991fa894caa90b16e048e77a2579362
RheaM <rheamarie-@live.no>
2021-05-04 12:38:48 +0200

    Initial commit

---
+++ app/src/main/java/com/example/oishii/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.oishii
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+//TODO: Minimum 3 Fragments
+//TODO: 
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
